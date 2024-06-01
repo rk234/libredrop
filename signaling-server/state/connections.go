c2097b87d68dd1968b26c886d36dc070d41f579f
Ramy Kaddouri <myramkadd@gmail.com>
2024-06-01 11:30:19 -0400

    add file

---
+++ signaling-server/state/connections.go
@@ -0,0 +1,28 @@
+package state
+
+type Peer struct {
+	ID          string
+	DisplayName string
+}
+
+var connections []Peer
+
+func Connect(peer Peer) {
+	connections = append(connections, peer)
+}
+
+func Disconnect(peer Peer) {
+	var index int = -1
+	for i := 0; i < len(connections); i++ {
+		if connections[i].ID == peer.ID {
+			index = i
+			break
+		}
+	}
+
+	if index != -1 {
+		connections = append(connections[:index], connections[index+1:]...)
+		RemoveOffer(peer.ID)
+		RemoveAnswer(peer.ID)
+	}
+}
