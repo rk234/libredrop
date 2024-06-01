7fbd3f4f217768eac37084b23a5ca1b80178df10
Ramy Kaddouri <myramkadd@gmail.com>
2024-05-31 20:51:22 -0400

    some ui work

--- libredrop-web/src/App.vue
+++ libredrop-web/src/App.vue
@@ -1,20 +1,31 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { ref, watch } from 'vue';
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
+const currentPage = ref(route.name)
+
+watch(route, () => currentPage.value = route.name)
 </script>
 
 <template>
-  <div class="w-full h-full p-8 flex flex-col items-center bg-gray-950 text-gray-200">
-    <div class="w-full max-w-2xl border-2 rounded border-emerald-900">
+  <div class="w-full h-full py-8 px-2 flex flex-col items-center bg-gray-950 text-gray-200">
+    <div class="w-full max-w-2xl">
       <header class="p-2">
+        <div class="mb-8">
+          <h1 class="text-4xl font-bold mb-1">Libredrop</h1>
+          <p>Simple peer-to-peer file sharing</p>
+        </div>
         <nav class="flex flex-row gap-2">
-          <RouterLink class="flex-1 text-center p-2 bg-emerald-800 rounded" to="/send"
-            >Send</RouterLink
-          >
-          <RouterLink class="flex-1 text-center p-2 rounded" to="/receive">Receive</RouterLink>
+          <RouterLink class="transition flex-1 text-center p-2 rounded border border-emerald-900 hover:bg-emerald-700"
+            :class="{ 'bg-emerald-700': currentPage == 'send' }" to="/send">
+            Send</RouterLink>
+          <RouterLink class="transition flex-1 text-center p-2 rounded border border-emerald-900 hover:bg-emerald-700"
+            :class="{ 'bg-emerald-700': currentPage == 'receive' }" to="/receive">Receive</RouterLink>
         </nav>
       </header>
 
-      <div class="p-2 border-t-2 border-emerald-900">
+      <div class="p-2">
         <RouterView />
       </div>
     </div>
--- libredrop-web/src/views/SendView.vue
+++ libredrop-web/src/views/SendView.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 
-onMounted(() => {
-  console.log('Mounted!')
-})
+function handleDrop(event: DragEvent) {
+  if (!event.dataTransfer) return;
+
+  if (event.dataTransfer.items) {
+    [...event.dataTransfer.items].forEach(async (item, i) => {
+      if (item.kind == "file") {
+        const file = item.getAsFile()
+        console.log(file)
+        console.log(await file?.arrayBuffer())
+      }
+    })
+  } else {
+    [...event.dataTransfer.files].forEach(async (file, i) => {
+      console.log(file)
+      console.log(await file.arrayBuffer())
+    })
+  }
+}
 </script>
 
 <template>
-  <main>HOME!</main>
+  <div class="flex flex-col gap-4">
+    <div @drop.prevent="handleDrop" @dragenter.prevent @dragover.prevent
+      class="border-2 min-h-52 border-dashed rounded-md border-gray-800 p-4 flex items-center justify-center">
+      <h2>Drop files here</h2>
+    </div>
+    <p class="text-center text-lg">or</p>
+    <textarea class="bg-gray-950 border-2 border-gray-800 p-2 min-h-28 text-gray-200 rounded-md"
+      placeholder="Enter text to send"></textarea>
+
+    <div class="flex flex-row gap-2">
+      <input class="flex-1 rounded bg-gray-800 p-2" type="text" placeholder="Enter receiver ID" />
+      <button class="bg-emerald-600 p-2 rounded">Send</button>
+    </div>
+  </div>
 </template>
