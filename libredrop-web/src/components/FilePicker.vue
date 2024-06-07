b47e44fe134313f15dbbb0c75797d2b025a12d39
Ramy Kaddouri <myramkadd@gmail.com>
2024-06-07 14:33:34 -0400

    file picker work

---
+++ libredrop-web/src/components/FilePicker.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from "vue"
+type Props = {
+  class: string
+}
+
+const props = defineProps<Props>()
+const files = ref<File[]>([])
+const inputBox = ref<HTMLInputElement>()
+
+const emit = defineEmits<{
+  (e: "filesUploaded", files: File[]): void
+}>()
+
+function handleDrop(event: DragEvent) {
+  if (!event.dataTransfer) return
+
+  if (event.dataTransfer.items) {
+    [...event.dataTransfer.items].forEach(async (item, _) => {
+      if (item.kind == 'file') {
+        const file = item.getAsFile()
+        if (file) {
+          console.log("Loaded file " + file.name)
+          files.value.push(file)
+        }
+      }
+    })
+  } else {
+    [...event.dataTransfer.files].forEach(async (file, _) => {
+      if (file) {
+        console.log("Loaded file " + file.name)
+        files.value.push(file)
+      }
+    })
+  }
+  emit("filesUploaded", files.value)
+}
+
+function handleInputElementChange(event: Event) {
+  const uploadedFiles = (event.target!! as HTMLInputElement).files
+  if (uploadedFiles) {
+    for (var i = 0; i < uploadedFiles.length; i++) {
+      if (uploadedFiles.item(i)) {
+        files.value.push(uploadedFiles.item(i)!!)
+      }
+    }
+    emit("filesUploaded", files.value)
+  }
+}
+</script>
+
+<template>
+  <div @drop.prevent="handleDrop" @dragenter.prevent @dragover.prevent @click="() => inputBox?.click()"
+    class="border-2 min-h-52 border-dashed rounded-md border-gray-800 p-4 flex items-center cursor-pointer justify-center"
+    :class="props.class">
+    <h2>Drop files here or click to select files!</h2>
+    <input ref="inputBox" multiple v-on:change="handleInputElementChange" type="file" class="hidden" />
+  </div>
+</template>
--- libredrop-web/src/views/SendView.vue
+++ libredrop-web/src/views/SendView.vue
@@ -2,28 +2,11 @@
 import { me } from '@/services/peer'
 import { SignalingChannel, type Answer } from '@/services/signaling'
 import { inject, ref, type Ref } from 'vue'
+import FilePicker from "../components/FilePicker.vue"
 
 const receiverID = ref<string>('')
 const rtcPeerConnection = inject<Ref<RTCPeerConnection>>('rtcConnection')
-
-function handleDrop(event: DragEvent) {
-  if (!event.dataTransfer) return
-
-  if (event.dataTransfer.items) {
-    ;[...event.dataTransfer.items].forEach(async (item, i) => {
-      if (item.kind == 'file') {
-        const file = item.getAsFile()
-        console.log(file)
-        console.log(await file?.arrayBuffer())
-      }
-    })
-  } else {
-    ;[...event.dataTransfer.files].forEach(async (file, i) => {
-      console.log(file)
-      console.log(await file.arrayBuffer())
-    })
-  }
-}
+const files = ref<File[]>([])
 
 function handleAnswer(answer: Answer) {
   console.log('Received answer!')
@@ -66,31 +49,21 @@ async function handleSend() {
     })
   }
 }
+
+function handleFiles(files: File[]) {
+  console.log(files)
+}
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
-    <div
-      @drop.prevent="handleDrop"
-      @dragenter.prevent
-      @dragover.prevent
-      class="border-2 min-h-52 border-dashed rounded-md border-gray-800 p-4 flex items-center justify-center"
-    >
-      <h2>Drop files here</h2>
-    </div>
+    <FilePicker @filesUploaded="handleFiles" class="" />
     <p class="text-center text-lg">or</p>
-    <textarea
-      class="bg-gray-950 border-2 border-gray-800 p-2 min-h-28 text-gray-200 rounded-md"
-      placeholder="Enter text to send"
-    ></textarea>
+    <textarea class="bg-gray-950 border-2 border-gray-800 p-2 min-h-28 text-gray-200 rounded-md"
+      placeholder="Enter text to send"></textarea>
 
     <div class="flex flex-row gap-2">
-      <input
-        v-model="receiverID"
-        class="flex-1 rounded bg-gray-800 p-2"
-        type="text"
-        placeholder="Enter receiver ID"
-      />
+      <input v-model="receiverID" class="flex-1 rounded bg-gray-800 p-2" type="text" placeholder="Enter receiver ID" />
       <button class="bg-emerald-600 p-2 rounded" @click="handleSend">Send</button>
     </div>
   </div>
