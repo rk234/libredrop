0dd7399884a4be6714586959177083d43c972b3e
Ramy K <myramkadd@gmail.com>
2024-06-07 22:45:35 -0400

    protocol work

--- libredrop-web/src/services/sendProtocol.ts
+++ libredrop-web/src/services/sendProtocol.ts
@@ -1,9 +1,27 @@
-
 export function sendFile(file: File, dataChannel: RTCDataChannel) {
+  const fileReader = new FileReader()
 
+  dataChannel.send(createFileStartMessage(file.name, file.type))
+
+  fileReader.addEventListener('error', (err) => console.log('ERROR: ', err))
+  fileReader.addEventListener('abort', (err) => console.log('ABORT: ', err))
+  fileReader.addEventListener('load', (evt) => {
+    if (evt.target) {
+      const fileData = evt.target.result as ArrayBuffer
+      dataChannel.send(createFileDataMessage(fileData))
+    }
+  })
 }
 
-function createFileStartMessage(filename: string, fileExtension: string): ArrayBuffer {
+function createFileDataMessage(chunk: ArrayBuffer): ArrayBuffer {
+  const buf = new ArrayBuffer(1 + 4 + chunk.byteLength)
+  const view = new DataView(buf)
+  // TODO
+
+  return buf
+}
+
+function createFileStartMessage(filename: string, fileType: string): ArrayBuffer {
+  // TODO
   return new ArrayBuffer(0)
 }
-
