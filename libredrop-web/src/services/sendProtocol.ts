526fed29800eab1a9ee6391e7116d2b8d5e2e0f2
Ramy Kaddouri <myramkadd@gmail.com>
2024-06-07 22:05:40 -0400

    add file

---
+++ libredrop-web/src/services/sendProtocol.ts
@@ -0,0 +1,9 @@
+
+export function sendFile(file: File, dataChannel: RTCDataChannel) {
+
+}
+
+function createFileStartMessage(filename: string, fileExtension: string): ArrayBuffer {
+  return new ArrayBuffer(0)
+}
+
